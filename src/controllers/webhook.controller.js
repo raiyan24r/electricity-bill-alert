34bc959c68cfaa67656825fe4db979679b09c2e7
unknown <raiyan24r@gmail.com>
2023-01-08 01:37:37 +0600

    fix axios

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,6 +1,7 @@
 const webhookService = require("../services/webhook.service");
 const messengerAppService = require("../services/messengerApp.service");
-const axios = require("axios").default;
+// const axios = require("axios").default;
+const basicHttp = require("../utils/basicHttp");
 const dotenv = require("dotenv").config();
 
 function handle(req, res) {
@@ -8,7 +9,7 @@ function handle(req, res) {
     webhookService.verifyCallback(req, res);
   } else if (req.method === "POST") {
     console.log("msg received");
-    axios
+    basicHttp
       .post(process.env.LOCAL_LOG_ROUTE + "/log", req.body)
       .then(function (response) {
         res.send(response);
@@ -18,14 +19,13 @@ function handle(req, res) {
       });
     res.send("dfdf");
     let body = req.body;
-    // if (body.object === "page") {
-    //   console.log("11");
-    //   messengerAppService.sendTextMessage(
-    //     process.env.TEST_PSID,
-    //     "Hello, world!"
-    //   );
-    //   messengerAppService.sendAlertNotificationRequest(process.env.TEST_PSID);
-    // }
+    if (body.object === "page") {
+      messengerAppService.sendTextMessage(
+        process.env.TEST_PSID,
+        "Hello, world!"
+      );
+      messengerAppService.sendAlertNotificationRequest(process.env.TEST_PSID);
+    }
 
     res.sendStatus(200);
   }
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,8 @@
 const axios = require("axios").default;
 const dotenv = require("dotenv").config();
 
-// const http = axios.create({
-//   baseURL: process.env.FB_GRAPH_URL,
-// });
+const http = axios.create({
+  baseURL: process.env.FB_GRAPH_URL,
+});
 
-// module.exports = http;
+module.exports = http;
