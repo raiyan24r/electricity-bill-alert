d57c1bb0ccd6ff4fda1bfdb91c7727d6ae3e244e
unknown <raiyan24r@gmail.com>
2023-01-08 20:46:10 +0600

    sds

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -4,27 +4,37 @@ const logHttp = require("../utils/http").logHttp;
 const dotenv = require("dotenv").config();
 
 function handle(req, res) {
-  if (req.method === "GET") {
-    webhookService.verifyCallback(req, res);
-  } else if (req.method === "POST") {
-    console.log("msg received");
+  try {
+    if (req.method === "GET") {
+      webhookService.verifyCallback(req, res);
+    } else if (req.method === "POST") {
+      console.log("msg received");
+      logHttp
+        .post(process.env.LOCAL_LOG_ROUTE + "/log", req.body)
+        .then(function (response) {
+          console.log("log sent");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+      let body = req.body;
+      if (body.object === "page") {
+        messengerAppService.sendTextMessage(
+          process.env.TEST_PSID,
+          "Hello, world!"
+        );
+        // messengerAppService.sendAlertNotificationRequest(process.env.TEST_PSID);
+      }
+    }
+  } catch (err) {
     logHttp
-      .post(process.env.LOCAL_LOG_ROUTE + "/log", req.body)
+      .post(process.env.LOCAL_LOG_ROUTE + "/log", JSON.stringify(err))
       .then(function (response) {
-        console.log("log sent");
-      })
-      .catch(function (error) {
-        console.log(error);
+        console.log("error");
       });
 
-    let body = req.body;
-    if (body.object === "page") {
-      messengerAppService.sendTextMessage(
-        process.env.TEST_PSID,
-        "Hello, world!"
-      );
-      messengerAppService.sendAlertNotificationRequest(process.env.TEST_PSID);
-    }
+    res.sendStatus(200);
   }
 }
 
